6c08da24a6118db0bc4da7275de8206b3966fba9
minjai100111 <minjai100111@gmail.com>
2025-03-13 18:55:23 +0900

    feature: create example bash operator dag

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,21 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    t1_orange = BashOperator(
+        task_id="t1_orange",
+        bash_command="/opt/airflow/plugins/select_fruit.sh ORANGE",
+    )
+
+    t2_avocado = BashOperator(
+        task_id="t2_avocado",
+        bash_command="/opt/airflow/plugins/select_fruit.sh AVOCADO",
+    )
+
+    t1_orange >> t2_avocado
